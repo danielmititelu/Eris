cf8d789650f18984e7783ec89a8382efd836db1e
Dumitru Daniel Mititelu <mttl_daniel@yahoo.com>
2017-11-04 12:59:37 +0200

    Rename fetchdata component to todo component

--- Eris/ClientApp/app/app.module.shared.ts
+++ Eris/ClientApp/app/app.module.shared.ts
@@ -7,15 +7,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { TodoComponent } from './components/fetchdata/todo.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
+        TodoComponent,
         HomeComponent
     ],
     imports: [
@@ -25,8 +23,7 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'to-do', component: TodoComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ Eris/ClientApp/app/models/todoItem.ts
@@ -0,0 +1,5 @@
+﻿interface TodoItem {
+    id: number;
+    name: string;
+    isCompleted: boolean;
+}
